c089f59538874eb27e210f5b34409816b939e5b3
Robisx <robertxas02@gmail.com>
2017-05-14 23:16:53 +0300

    blob sutvarkytas

--- src/java/lt/svako/stud/robertasjarockis/baigiamasiswebapp/utils/Utils.java
+++ src/java/lt/svako/stud/robertasjarockis/baigiamasiswebapp/utils/Utils.java
@@ -5,11 +5,15 @@
  */
 package lt.svako.stud.robertasjarockis.baigiamasiswebapp.utils;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
+import com.google.gson.JsonDeserializationContext;
+import java.lang.reflect.Type;
+import java.util.Date;
 import java.util.List;
 import lt.svako.stud.robertasjarockis.baigiamasiswebapp.entities.Vartotojas;
 import lt.svako.stud.robertasjarockis.baigiamasiswebapp.entities.Vieta;
 import lt.svako.stud.robertasjarockis.baigiamasiswebapp.managers.VartotojasManager;
+import org.apache.commons.codec.binary.Base64;
 
 /**
  *
@@ -30,13 +34,28 @@ public class Utils {
 
     public static Vartotojas parseVartotojas(String parse) {
         Gson gson = new Gson();
-        System.out.println(parse);
         return gson.fromJson(parse, Vartotojas.class);
     }
 
     public static Vieta parseVieta(String parse) {
-        Gson gson = new Gson();
-        System.out.println(parse);
+        GsonBuilder builder = new GsonBuilder();
+       GsonHelper gh = new GsonHelper();
+// Adapter to convert a Unix Long Time in Json to a java.util.Date object
+        builder.registerTypeAdapter(byte[].class, new JsonDeserializer<byte[]>() {
+            public byte[] deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                return Base64.decodeBase64(json.getAsJsonPrimitive().getAsString());
+            }
+        });
+
+// Adapter to convert byte[] in a Java Object to a String in Json for UI to display image properly
+        builder.registerTypeAdapter(byte[].class, new JsonSerializer<byte[]>() {
+            public JsonElement serialize(byte[] src, Type typeOfSrc, JsonSerializationContext context) {
+                return new JsonPrimitive(Base64.encodeBase64String(src));
+            }
+        });
+
+        Gson gson = builder.create();
+       // gson = new Gson();
         return gson.fromJson(parse, Vieta.class);
     }
 }
